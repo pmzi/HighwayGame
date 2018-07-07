597c0b0dd9964511fcde31db930b15e451fb330a
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-07-07 21:03:22 +0430

    road setting modal concept changed

--- src/sample/PageBuilder/RoadSettingsModal.java
+++ src/sample/PageBuilder/RoadSettingsModal.java
@@ -1,25 +1,29 @@
 package sample.PageBuilder;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * Created by pmzi on 7/7/2018.
  */
 public class RoadSettingsModal {
 
-    public <T> T show(){
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(getClass().getResource("../Views/roadSettingsModal.fxml"));
-        T controller = fxmlLoader.getController(); // Retrieve the controller
-        try{
+    public static Object show() {
+        RoadSettingsModal roadSettingsModal = new RoadSettingsModal();
+        return roadSettingsModal.showPage();
+    }
 
-            Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+    private Object showPage() {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(this.getClass().getResource("../Views/roadSettingsModal.fxml"));
+        Object controller = fxmlLoader.getController();
+
+        try{
+            Scene scene = new Scene((Parent)fxmlLoader.load(), 400.0D, 300.0D);
             Stage stage = new Stage();
             stage.setTitle("Road Settings");
             scene.getStylesheets().add("sample/assets/css/modal.css");
@@ -28,12 +32,9 @@ public class RoadSettingsModal {
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.showAndWait();
             return controller;
-        }catch (IOException e){
-            e.printStackTrace();
+        }catch (Exception e){
+            return controller;
         }
-
-        return controller;
-
     }
 
 }
