cad266a8251d7ef49ed65b2b6086a7279ad53f98
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-07-07 21:02:35 +0430

    icon added to load page

--- src/sample/PageBuilder/LoadPage.java
+++ src/sample/PageBuilder/LoadPage.java
@@ -2,7 +2,6 @@ package sample.PageBuilder;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -23,7 +22,6 @@ public class LoadPage {
             Scene scene = new Scene(fxmlLoader.load(), 600, 500);
             Stage stage = new Stage();
             stage.setTitle("Load a game");
-            stage.getIcons().add(new Image("sample/assets/images/icon.png"));
             scene.getStylesheets().add("sample/assets/css/main.css");
             stage.setScene(scene);
             stage.show();
