4fa5e36a10368a3ba5625b0a6bfe9748336d0641
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-07-07 12:35:52 +0430

    save modal is now operational

--- src/sample/PageBuilder/SaveModal.java
+++ src/sample/PageBuilder/SaveModal.java
@@ -20,7 +20,7 @@ public class SaveModal {
              * if "fx:controller" is not set in fxml
              * fxmlLoader.setController(NewWindowController);
              */
-            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            Scene scene = new Scene(fxmlLoader.load(), 300, 220);
             Stage stage = new Stage();
             stage.setTitle("Save Modal");
             scene.getStylesheets().add("sample/assets/css/modal.css");
